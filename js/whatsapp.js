c0ad376e5efa3d1cc4f8c2d9b2cf6ac501ddbb3f
sodiqlef <kehindeogundimu731@gmail.com>
2023-03-30 12:20:09 +0100

    text wharsapp link

---
+++ js/whatsapp.js
@@ -0,0 +1,17 @@
+  function whatsapp() {
+      var first = document.getElementById("first").value;
+      var last = document.getElementById("last").value;
+      var email = document.getElementById("email").value;
+      var phone = document.getElementById("phone").value;
+      var date = document.getElementById("date").value;
+      var message = document.getElementById("message").value;
+
+      var url = "https://wa.me/2348165994421?text=" +
+          "*Name :* " + first + ' ' + last + "%0a" +
+          "*Email :* " + email + "%0a" +
+          "*Phone :* " + phone + "%0a" +
+          "*Date :* " + date + "%0a" +
+          "*Message :* " + message;
+
+      window.open(url, '_blank').focus();
+  }
