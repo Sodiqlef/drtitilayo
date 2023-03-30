9b2007547cff7cfa093abe6064b921dfdebc328c
sodiqlef <kehindeogundimu731@gmail.com>
2023-03-30 14:56:53 +0100

    testimonial commit

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -3,14 +3,12 @@
       var last = document.getElementById("last").value;
       var email = document.getElementById("email").value;
       var phone = document.getElementById("phone").value;
-      var date = document.getElementById("date").value;
       var message = document.getElementById("message").value;
 
-      var url = "https://wa.me/2348165994421?text=" +
+      var url = "https://wa.me/2349024057678?text=" +
           "*Name :* " + first + ' ' + last + "%0a" +
           "*Email :* " + email + "%0a" +
           "*Phone :* " + phone + "%0a" +
-          "*Date :* " + date + "%0a" +
           "*Message :* " + message;
 
       window.open(url, '_blank').focus();
